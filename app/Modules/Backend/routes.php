7730dcccd4132616eddc07cc4b832911e39a0cd6
thuanlv <levanthuankmhd@gmail.com>
2018-11-28 00:01:32 +0700

    login function

--- app/Modules/Backend/routes.php
+++ app/Modules/Backend/routes.php
@@ -1,9 +1,26 @@
 <?php
 
-Route::group(['module'=>'Frontend'], function(){
+Route::group(['prefix'=>'admin', 'module'=>'Backend'], function(){
 
-    Route::get('/admin', function(){
-        echo "Welcome admin";
+    Route::get('/test-user', function(){
+
+//        $user = new \App\Models\User();
+//
+//        $user->name = 'levanthuan';
+//        $user->email = 'levanthuankmhd@gmail.com';
+//        $user->password = bcrypt('123456');
+//        $user->name_display = 'le van thuan';
+//        $user->birthday = '2018-11-27';
+//        $user->address = 'Hai Duong';
+//        $user->phone_number = '928592523';
+//        $user->role = 0;
+//
+//        $user->save();
+
+        $list = \App\Models\User::all();
+
+        dd($list->toArray());
+
     });
 
 });
