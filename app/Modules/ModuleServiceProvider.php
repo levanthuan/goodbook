5d16a62fca9e48fb5c07b8491956227c4021e305
thuanlv <levanthuankmhd@gmail.com>
2018-11-27 21:21:58 +0700

    module app frontend and backend

---
+++ app/Modules/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php   namespace App\Modules;
+
+use Illuminate\Support\ServiceProvider;
+use Request;
+
+class ModuleServiceProvider extends ServiceProvider{
+
+    public function register(){}
+
+    public function boot(){
+        // Load modules in config.module file
+        $modules = config('module.modules');
+
+        $mod = $modules['site'];
+        //Detect backend route ?
+        if(Request::is('admin') || Request::is('admin/*')){
+            $mod = $modules['admin'];
+        }
+
+        //Load file route.php with other module
+        if(file_exists(__DIR__ . '/' . $mod . '/routes.php')){
+            include __DIR__ . '/' . $mod . '/routes.php';
+        }
+
+        //Load template with other module
+        if(is_dir(__DIR__ . '/' . $mod . '/Views')){
+            $this->loadViewsFrom(__DIR__ . '/' . $mod . '/Views', $mod);
+        }
+    }
+}
